52985f4e26463f3660fab64db23a3e9e673da8fd
MemazoG <memogarcia246@gmail.com>
2021-11-10 00:06:28 -0600

    change button name

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -81,7 +81,7 @@ class Register extends React.Component {
                                 </div>
                             </fieldset>
                             <div className="">
-                                <input onClick={this.onSubmitRegister} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
+                                <input onClick={this.onSubmitRegister} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Register" />
                             </div>
                         </div>
                     </main>
